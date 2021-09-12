e37fd489d3c44574000f820e0835be29fc628a90
sebowykubiak <sebowykubiak@gmail.com>
2021-09-12 17:22:59 +0200

    feat(errors): Export errors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import { ConcurrentQueueFactory } from "./ConcurrentQueueFactory";
-import { IConcurrentQueueFactory } from "./interfaces/IConcurrentQueueFactory";
-import { IConcurrentQueue, IConcurrentQueueParams } from "./interfaces/IConcurrentQueue";
-import { ConcurrentQueue } from "./ConcurrentQueue";
+import { ConcurrentQueueFactory } from './ConcurrentQueueFactory';
+import { IConcurrentQueueFactory } from './interfaces/IConcurrentQueueFactory';
+import { IConcurrentQueue, IConcurrentQueueParams } from './interfaces/IConcurrentQueue';
+import { ConcurrentQueue } from './ConcurrentQueue';
+import { ConcurrentQueueError } from './errors/ConcurrentQueueError';
+import { MaxConcurrencyLessThanOneError } from './errors/MaxConcurrencyLessThanOneError';
 
-const createConcurrentQueue = (params: IConcurrentQueueParams) => {
-    const factory = new ConcurrentQueueFactory();
-    return factory.createConcurrentQueue(params);
+const createConcurrentQueue = (params: IConcurrentQueueParams): IConcurrentQueue => {
+  const factory = new ConcurrentQueueFactory();
+  return factory.createConcurrentQueue(params);
 };
 
 export { createConcurrentQueue, ConcurrentQueue, IConcurrentQueue, IConcurrentQueueParams };
 export { ConcurrentQueueFactory, IConcurrentQueueFactory };
+export { ConcurrentQueueError, MaxConcurrencyLessThanOneError };
 
 export default createConcurrentQueue;
